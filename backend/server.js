15bad577f346c8f9f83c275909598f9227cc6a5e
Piku <lipsuuu785@gmail.com>
2025-09-14 15:10:31 +0530

    project completed

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express=require("express")
+const app=express()
+const dotenv=require("dotenv").config()
+const connectDb=require("./config/connectionDb")
+const cors=require("cors")
+const path=require("path")
+
+const PORT=process.env.PORT || 3000
+const __dirname = path.resolve();
+connectDb()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static("public"))
+
+app.use("/",require("./routes/user"))
+app.use("/recipe",require("./routes/recipe"))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT,(err)=>{
+    console.log(`app is listening on port ${PORT}`)
+})
